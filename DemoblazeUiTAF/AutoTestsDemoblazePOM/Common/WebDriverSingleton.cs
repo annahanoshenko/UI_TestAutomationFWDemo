9933fccba4f0eb5af79574bb9f61bd88f0f251c6
Anna Hanoshenko <49571341+annahanoshenko@users.noreply.github.com>
2025-03-31 10:03:42 +0200

    Added Demoblaze website UI automation. (#1)
    * Added Demoblaze website UI automation.
    
    * created class called UserStorage and add all of the users in the tests to the storage
    
    * Added checking of the result value in tests
    
    * Created class called EnvironmentConfig and put website URL there
    
    * Created different tests classes based on the feature we are testing:
    RegistrationTests
    LoginTests
    OrderTests
    
    * Created a builder class for the OrderEntity.
    Then created OrdersStorage and use builder there to create OrderObjects
    
    * Used Singleton pattern here to ensure we have only one point of access for our WebDriver
    
    * 1)  Created different tests classes in the "DragAndDropGlobalsqa":
    DeleteImageTests
    RecycleImageTests
    ZoomImageTests
    
    2)made consistent and call variable isElementInPhotoManager
    
    ---------
    
    Co-authored-by: Anna Hanoshenko <hanoshenkoanna@gmail.com>

---
+++ DemoblazeUiTAF/AutoTestsDemoblazePOM/Common/WebDriverSingleton.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+
+namespace DemoblazeUiTAF.AutoTestsDemoblazePOM.Common
+{
+    internal class WebDriverSingleton
+    {
+        private static IWebDriver _instance;
+
+        private WebDriverSingleton() { }
+
+        public static IWebDriver Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new OpenQA.Selenium.Chrome.ChromeDriver();
+                }
+                return _instance;
+            }
+        }
+        public static void QuitDriver()
+        {
+            if (_instance != null)
+            {
+                _instance.Quit();
+                _instance = null;
+            }
+        }
+    }
+}
---
+++ DemoblazeUiTAF/AutoTestsDemoblazePOM/Pages/SignUpPage.cs
@@ -0,0 +1,31 @@
+﻿using DemoblazeUiTAF.AutoTestsDemoblazePOM.Base;
+using DemoblazeUiTAF.AutoTestsDemoblazePOM.Entities;
+using OpenQA.Selenium;
+
+namespace DemoblazeUiTAF.AutoTestsDemoblazePOM.Pages
+{
+    internal class SignUpPage : PageBase
+    {
+        private IWebElement SignUpBtn => Driver.FindElement(By.Id("signin2"));
+        private IWebElement UserNameTextField => Driver.FindElement(By.Id("sign-username"));
+        private IWebElement PasswordTextField => Driver.FindElement(By.Id("sign-password"));
+        private IWebElement SignUpOnPopUpBtn => Driver.FindElement(By.XPath("//button[text() = 'Sign up']"));
+
+        public SignUpPage(IWebDriver driver) : base(driver)
+        {
+        }
+
+        public void ClickSignUpButton() => SignUpBtn.Click();
+        public void EnterUserName(string username) => UserNameTextField.SendKeys(username);
+        public void EnterPassword(string password) => PasswordTextField.SendKeys(password);
+        public void ClickSignUpOnPopUpButton() => SignUpOnPopUpBtn.Click();
+
+        public void RegisterNewUser(UserEntity user)
+        {
+            ClickSignUpButton();
+            EnterUserName(user.UserName);
+            EnterPassword(user.Password);
+            ClickSignUpOnPopUpButton();
+        }
+    }
+}
